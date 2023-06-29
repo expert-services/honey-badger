a6830a5688a7669d4d618e0415b9e4c8515a1f2b
David Wiggs <107562400+david-wiggs@users.noreply.github.com>
2023-06-29 22:42:13 +0000

    Try to get workflow file

--- src/index.ts
+++ src/index.ts
@@ -14,12 +14,18 @@ _,-='=- =-  -'"--.__,,.._
                         '>>>
                         `
 export = async (app: Probot) => {
-  app.on("deployment_protection_rule" as any, async (context: any) => {
+  app.on("deployment_protection_rule" as any, async (context: any) => {    
     app.log.debug(`Received Webhook ${JSON.stringify(context)}`)
     const regex = /[^0-9]*([0-9]*).*$/g;
     const matches = regex.exec(context.payload.deployment_callback_url)
     context.payload.deployment_callback_url.match(regex);
     const runId = matches?.[1];
+
+    // Get the workflow file for runID
+    const workflow = await getWorkflow(app, context, runId);
+
+    // Log the contents of workflow
+    app.log.info(`workflow: ${workflow}`)
 
     const result = Math.random() * 10;
     if (result < 5) {
@@ -64,4 +70,20 @@ async function rejectWorkflow(app: Probot, context: any, run_id: string | undefi
     app.log(error)
   }
 }
+
+// An async function that downloads the workflow file from a specific run
+async function getWorkflow(app: Probot, context: any, run_id: string | undefined) {
+  app.log.info(`Getting workflow! ${run_id}`);
+  try {
+    const workflow = await context.octokit.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}/workflow_file', context.repo({
+      run_id,
+    }))
+    app.log.info(`workflow: ${workflow}`)
+    return workflow
+  } catch (error: any) {
+    app.log(error)
+  }
+}
+
+// Using the payload from the webhook, use the run_id to get the workflow file
 
