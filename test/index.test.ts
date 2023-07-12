1489b4cd5439d0f9bfe2a1292164b5f93b1e0326
David Wiggs <david-wiggs@github.com>
2023-07-12 18:16:37 -0500

    Add a simple accepted workflow test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,13 +1,10 @@
-// You can import your modules
-// import index from '../src/index'
-
 import nock from "nock";
-// Requiring our app implementation
 import myProbotApp from "../src";
 import { Probot, ProbotOctokit } from "probot";
-// Requiring our fixtures
-import payload from "./fixtures/issues.opened.json";
-const issueCreatedBody = { body: "Thanks for opening this issue!" };
+import payload from "./fixtures/deployment_protection_rule.requested.json";
+import run from "./fixtures/deployment_run.5417285903.json";
+import workflow from "./fixtures/workflow.codeql-analysis.json";
+const deploymentAcceptedBody = { body: "Deployment was accepted!" };
 const fs = require("fs");
 const path = require("path");
 
@@ -16,7 +13,7 @@ const privateKey = fs.readFileSync(
   "utf-8"
 );
 
-describe("My Probot app", () => {
+describe("honey-badger", () => {
   let probot: any;
 
   beforeEach(() => {
@@ -34,41 +31,34 @@ describe("My Probot app", () => {
     probot.load(myProbotApp);
   });
 
-  test("creates a comment when an issue is opened", async (done) => {
+  test("accepts a deployment when a deployment protection rule is triggered", async () => {
     const mock = nock("https://api.github.com")
       // Test that we correctly return a test token
-      .post("/app/installations/2/access_tokens")
+      .post("/app/installations/39129914/access_tokens")
       .reply(200, {
         token: "test",
         permissions: {
-          issues: "write",
+          deployments: "write",
         },
       })
 
-      // Test that a comment is posted
-      .post("/repos/hiimbex/testing-things/issues/1/comments", (body: any) => {
-        done(expect(body).toMatchObject(issueCreatedBody));
-        return true;
-      })
-      .reply(200);
+      .get("/repos/oodles-noodles/juice-shop/actions/runs/5417285903")
+      .reply(200, run)
 
-    // Receive a webhook event
-    await probot.receive({ name: "issues", payload });
+      .get("/repos/oodles-noodles/juice-shop/contents/.github%2Fworkflows%2Fcodeql-analysis.yml")
+      .reply(200, workflow)
+
+      .post("/repos/oodles-noodles/juice-shop/actions/runs/5417285903/deployment_protection_rule")
+      .reply(200, deploymentAcceptedBody)
+
+      // Receive a webhook event
+      await probot.receive({ name: "deployment_protection_rule", payload });
 
     expect(mock.pendingMocks()).toStrictEqual([]);
-  });
+  }, 20000 /* timeout in milliseconds */);
 
   afterEach(() => {
     nock.cleanAll();
     nock.enableNetConnect();
   });
 });
-
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
-
-// For more information about using TypeScript in your tests, Jest recommends:
-// https://github.com/kulshekhar/ts-jest
-
-// For more information about testing with Nock see:
-// https://github.com/nock/nock
